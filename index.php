5ce9da4ba7124722c481e78bc416427f62f0e9e1
anvarbeek <anvar1on007@gmail.com>
2024-08-03 22:30:30 +0500

    navbar qishmi tugadi

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <? include 'header.php'; ?>
+<div class="conteiner">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="conteiner-fluid">
+            <a href="#Logo" class="navbar-brand ms-2"><img src="https://upload.wikimedia.org/wikipedia/commons/5/52/Free_logo.svg" alt="Bootstrap" width="100" height="55"></a>
+        </div>
+        <div class="collapse justify-content-center navbar-collapse">
+            <ul class="nav nav-pills mb-2 " id="pills-tab" role="tablist">
+                <li class="nav-item"><a href="#home" class="nav-link active" id="pills-home-tab" role="presentation" aria-selected="true">Home</a></li>
+                <li class="nav-item"><a href="#about" class="nav-link " id="pills-about-tab" role="presentation" aria-selected="false">About</a></li>
+                <li class="nav-item"><a href="#home" class="nav-link " >Contact</a></li>
+            </ul>
+        </div>
+        <div class="justify-content-end">
+            <button type="submit" class="btn btn-outline-success m-2">Sign-in</button>
+        </div>
+    </nav>
+    <form action="login.php" method="post">
 
-<?php
-
-echo '<h1>salom</h1>';
-
-
-
-?>
-
-
+    </form>
+</div>
 <? include 'footer.php'; ?>
