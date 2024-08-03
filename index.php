7f0227cdca26ed5683fba818a923bb8f571d992e
anvarbeek <anvar1on007@gmail.com>
2024-08-04 01:12:16 +0500

    hali tugatilmadi

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
         </div>
         <div class="collapse justify-content-center navbar-collapse">
             <ul class="nav nav-pills mb-2 " id="pills-tab" role="tablist">
-                <li class="nav-item"><a href="#home" class="nav-link active" id="pills-home-tab" role="presentation" aria-selected="true">Home</a></li>
-                <li class="nav-item"><a href="#about" class="nav-link " id="pills-about-tab" role="presentation" aria-selected="false">About</a></li>
-                <li class="nav-item"><a href="#home" class="nav-link " >Contact</a></li>
+                <li class="nav-item col-sm"><a href="#home" class="nav-link active" id="pills-home-tab" role="presentation" aria-selected="true">Home</a></li>
+                <li class="nav-item col-sm"><a href="#about" class="nav-link " id="pills-about-tab" role="presentation" aria-selected="false">About</a></li>
+                <li class="nav-item col-sm"><a href="#home" class="nav-link ">Contact</a></li>
             </ul>
         </div>
         <div class="justify-content-end">
@@ -16,8 +16,28 @@
         </div>
     </nav>
     <section class="home" id="home">
-        
+        <div class="carousel slide" id="carouselExampleControls">
+            <div class="carousel-inner justify-content-center">
+                <div class="carousel-item active ">
+                    <img src="https://images.pexels.com/photos/5595519/pexels-photo-5595519.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" style="height: 90vh;" alt="internetni ulang" class="d-block w-100">
+                </div>
+                <div class="carousel-item ">
+                    <img src="https://images.pexels.com/photos/193021/pexels-photo-193021.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" style="height: 90vh;" alt="internetni ulang" class="d-block w-100">
+                </div>
+                <div class="carousel-item ">
+                    <img src="https://images.pexels.com/photos/6166016/pexels-photo-6166016.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" style="height: 90vh;" alt="internetni ulang" class="d-block w-100">
+                </div>
+            </div>
+            <button class="carousel-control-prev" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
     </section>
-    
+
 </div>
 <? include 'footer.php'; ?>
